b804543b44493e0553f9a30c953f5b506c1598bc
Hyurl <ayon@hyurl.com>
2015-12-06 01:07:28 +0800

    jQuery 判断客户端是否为手机浏览器 $.isMobile(agent)。
    $.isMobile(agent) 判断客户端浏览器是否为手机浏览器。
    agent: 客户端, 包括 Android, BlackBerry, iOS, Windows 和 any。

--- isMobile.js
+++ isMobile.js
@@ -1,38 +1,25 @@
 (function($){
   $.extend({
     /*
-			$.isMobile(agent, callback) 判断客户端浏览器是否为手机浏览器。
-			agent: 客户端, 包括 Android, BlackBerry, iOS, Windows 和 all。
-			callback: 回调函数, 但判断结果为真时执行的函数。
+			$.isMobile(agent) 判断客户端浏览器是否为手机浏览器。
+			agent: 客户端, 包括 Android, BlackBerry, iOS, Windows 和 any。
 		*/
-    isMobile: function(agent, callback){
+		isMobile: function(agent, callback){
 			switch(agent){
 				case 'Android':
-					if(navigator.userAgent.match(/Android/i)){
-						if(callback !=undefined && $.isFunction(callback)) callback();
-					}
+					return navigator.userAgent.match(/Android/i);
 				break;
 				case 'BlackBerry':
-					if(navigator.userAgent.match(/BlackBerry/i)){
-						if(callback !=undefined && $.isFunction(callback)) callback();
-					}
+					return navigator.userAgent.match(/BlackBerry/i);
 				break;
 				case 'iOS':
-					if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){
-						if(callback !=undefined && $.isFunction(callback)) callback();
-					}
+					return navigator.userAgent.match(/iPhone|iPad|iPod/i);
 				break;
 				case 'Windows':
-					if(navigator.userAgent.match(/IEMobile/i)){
-						if(callback !=undefined && $.isFunction(callback)) callback();
-					}
+					return navigator.userAgent.match(/IEMobile/i);
 				break;
-				case 'all':
-					if(navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|IEMobile/i)){
-						if(callback !=undefined && $.isFunction(callback)) callback();
-					}
-				break;
-				default:
+				case 'any':
+					return navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|IEMobile/i);
 				break;
 			}
 		}
