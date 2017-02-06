1efdb314c6c571c883b69ab79a2e80a9d1099df9
Hyurl <ayon@hyurl.com>
2017-02-06 22:19:40 +0800

    Add files via upload

--- jquery.ajaxsubmit.js
+++ jquery.ajaxsubmit.js
@@ -6,7 +6,7 @@
 	 *                    success: 回调函数（内部可用 $(this) 指向表单），表单提交成功时触发
 	 *                    beforeSend: 回调函数（内部可用 $(this) 指向表单），用于提交前进行验证表单，当返回 false 时将阻止表单提交
 	 *                    setSelector: 子集选择器名称，用于分组提交数据，通常在表单内使用 <fieldset> 标签进行分组
-	 *                    once: 当设置为 true 时，分组提交仅触发一次 success 回调函数；
+	 *                    once: 当设置为 true 时，分组提交仅触发一次 success 和 beforeSend 回调函数；
 	 *                    		你可以将服务器成功/失败的返回值设置为 1/0，表单提交完成后，success 的参数将被填充为提交成功的次数
 	 *                    除了上面的这些参数，其他参数与 $.ajax 相同，但是 url 和 type 会在优先从表单中中获取。
 	 * @return this
@@ -35,31 +35,38 @@
 			event.preventDefault();
 			options.url = $this.attr('action') || options.url;
 			options.type = $this.attr('method') || options.type;
-			if(typeof arg.beforeSend == 'function' && arg.beforeSend.call($this, options) === false){
-				return false;
-			}
-			var isCallable = typeof arg.success == 'function';
+			var successCallable = typeof arg.success == 'function',
+				beforeSendCallable = typeof arg.beforeSend == 'function';
 			if(arg.setSelector && $this.find(arg.setSelector).length){
 				var i = t = 0, length = $this.find(arg.setSelector).length;
+				if(beforeSendCallable && arg.once && arg.beforeSend.call($this, options) === false){
+					return false;
+				}
 				options.success = function(result){
 					if(isNaN(result)){
 						t = result;
 					}else{
 						t += parseInt(result);
 					}
-					if(isCallable){
-						if(!arg.once || i == length) arg.success.call($this, t);
+					if(successCallable){
+						if(!arg.once || i == length-1) arg.success.call($this, t);
 					}
 					i++;
 				};
 				$this.find(arg.setSelector).each(function(){
 					options.data = $(this).children().serialize();
+					if(beforeSendCallable && !arg.once && arg.beforeSend.call($this, options) === false){
+						return false;
+					}
 					$.ajax(options);
 				});
 			}else{
 				options.data = $this.serialize();
+				if(beforeSendCallable && arg.beforeSend.call($this, options) === false){
+					return false;
+				}
 				options.success = function(result){
-					if(isCallable) arg.success.call($this, result);
+					if(successCallable) arg.success.call($this, result);
 				};
 				$.ajax(options);
 			}
