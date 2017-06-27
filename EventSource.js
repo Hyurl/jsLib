81ff11479c4aef9ce365cff4de6e1ab13234679b
hyurl <ayon@hyurl.com>
2017-06-27 17:01:09 +0800

    Upload via git

--- EventSource.js
+++ EventSource.js
@@ -1,8 +1,8 @@
 (function(){
 	/**
-	 * EventSource() 创建一个事件源对象，用来弥补 IE/Edge 浏览器不支持 SSE 的缺陷。
-	 *               但需要注意的是，这个构造函数并不支持断线重连功能，并且在某些事件
-	 *               处理和浏览器自带的有所差别。因此程序会优先使用浏览器自带 EventSource 对象。
+	 * EventSource() 创建一个事件源对象，用来弥补 IE10+/Edge 浏览器不支持 SSE(Server-Sent Events) 的缺陷。
+	 *               但需要注意的是，这个构造函数并不支持断线重连功能，并且在某些事件处理
+	 *               和浏览器自带的有所差别。因此程序会优先使用浏览器自带 EventSource 对象。
 	 * @param {string} url           要访问的 URL 地址
 	 * @param {object} configuration 额外的配置，支持下面这些项目
 	 *                               · withCredentials: 在跨域请求时发送证书，默认 false
@@ -22,7 +22,7 @@
 		this.close = function(){
 			this.readyState = 2;
 			return xhr.abort();
-		}
+		};
 
 		/** 通过 XMLHttpRequest 来进行求情 */
 		var $this = this; //创建自身的引用
@@ -30,7 +30,8 @@
 		xhr.withCredentials = $this.withCredentials;
 		xhr.loaded = 0; //已加载数据长度
 		xhr.onreadystatechange = function(){
-			if(xhr.readyState === 1){ //连接打开
+			console.log(xhr);
+			if(xhr.readyState == 1){ //连接打开
 				$this.readyState = 0; //正在建立连接
 				if(typeof $this.onopen == 'function'){
 					var event = {
@@ -47,12 +48,12 @@
 						srcElement: $this,
 						target: $this,
 						timeStamp: (new Date()).getTime(),
-						type: "open",
-					}
+						type: "open"
+					};
 					$this.onopen(event); //触发 open 事件
 				}
-			}else if(xhr.status === 200){
-				if(xhr.readyState === 2){ //收到头部
+			}else if(xhr.status == 200){
+				if(xhr.readyState == 2){ //收到头部
 					var contentType = xhr.getResponseHeader('Content-Type').toLowerCase().split(';')[0];
 					if(contentType != 'text/event-stream'){ //服务器发送的 Content-Type 必须是 text/event-stream
 						console.error("EventSource's response has a MIME type (\""+contentType+"\") that is not \"text/event-stream\". Aborting the connection.");
@@ -73,24 +74,24 @@
 								srcElement: $this,
 								target: $this,
 								timeStamp: (new Date()).getTime(),
-								type: "error",
-							}
+								type: "error"
+							};
 							$this.onerror(event); //触发 error 事件
 						}
 					}
-				}else if(xhr.readyState === 3){ //接收消息
+				}else if(xhr.readyState == 3){ //接收消息
 					$this.readyState = 1; //连接持续中
 					var data = xhr.responseText.substring(xhr.loaded);
 					xhr.loaded = xhr.responseText.length;
 					if(typeof $this.onmessage == 'function'){
-						if(data.indexOf('data:') === 0){
+						if(data.indexOf('data:') == 0){
 							var event = {
 								bubbles: false,
 								cancelBubbles: false,
 								cancelable: false,
 								composed: false,
 								currentTarget: $this,
-								data: data.substring(5).replace(/(^\s*)|(\n\n$)/g, ""), //消息内容
+								data: data.substring(5).replace(/(^\s*)|([\r\n]*$)/g, ""), //消息内容
 								defaultPrevented: false,
 								eventPhase: 0,
 								isTrusted: true,
@@ -103,13 +104,13 @@
 								srcElement: $this,
 								target: $this,
 								timeStamp: (new Date()).getTime(),
-								type: "message",
+								type: "message"
 							};
 							$this.onmessage(event); //触发 message 事件
 						}
 					}
 				}
-			}else if(xhr.status !== 200 && xhr.loaded === 0 && xhr.readyState === 4){
+			}else if(xhr.status != 200 && xhr.loaded == 0 && xhr.readyState == 4){
 				$this.readyState = 2; //连接已关闭
 				xhr.abort();
 				if(typeof $this.onerror == 'function'){
@@ -127,8 +128,8 @@
 						srcElement: $this,
 						target: $this,
 						timeStamp: (new Date()).getTime(),
-						type: "error",
-					}
+						type: "error"
+					};
 					$this.onerror(event); //触发 error 事件
 				}
 			}
