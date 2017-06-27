e5cdff0533d797e1635a1cda89c1317f3b93eb84
hyurl <ayon@hyurl.com>
2017-06-28 03:10:34 +0800

    Update via git

--- require.js
+++ require.js
@@ -1,22 +1,37 @@
-var require = function(url, arg, callback){
-	var options = {
+/**
+ * require() 加载并运行一个外部脚本文件
+ * @param  {String}   url      js 文件源地址
+ * @param  {Object}   options  设置选项，支持下面这些参数
+ *                             · type      脚本 MIME 类型，默认 application/javascript
+ *                             · async     异步加载，默认 true
+ *                             · defer     脚本延迟执行，默认 false
+ *                             · cahrset   设置编码
+ *                             · innerText <script> 标签内容
+ * @param  {Function} callback 脚本加载完成后执行的回调函数
+ */
+function require(url, options, callback){
+	var _options = {
+			type: 'application/javascript',
 			async: true,
-			defer: true,
+			defer: false,
+			charset: "",
 			innerText: ""
 		};
 	var ele = document.createElement("script");
-	ele.src = url;
-	if(typeof arg == 'function'){
-		callback = arg;
-		arg = "";
-	}else if(typeof arg == 'object'){
-		for(var x in arg){
-			options[x] = arg[x];
+	if(typeof options == 'function'){
+		callback = options;
+		options = {};
+	}else if(typeof options == 'object'){
+		for(var x in options){
+			_options[x] = options[x];
 		}
 	}
-	if(options.async) ele.async = "async";
-	if(options.defer) ele.defer = "defer";
-	if(options.innerText) ele.innerText = options.innerText;
+	ele.type = _options.type;
+	ele.src = url;
+	if(_options.async) ele.async = "async";
+	if(_options.defer) ele.defer = "defer";
+	if(_options.charset) ele.charset = _options.cahrset;
+	if(_options.innerText) ele.innerText = options.innerText;
 	ele.addEventListener("load", function(){
 		callback.call(ele);
 	});
